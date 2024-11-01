ff48f411bbbef7a4c5cf462b1457b5684e48ca1b
Munoz, Emmanuel <emunoz31@gatech.edu>
2024-10-31 22:08:01 -0400

    days of the week update according to the django backend

--- p5calendar/views.py
+++ p5calendar/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from datetime import datetime, timedelta
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+
+    today = datetime.now()
+
+    # dates will be an empty array
+    dates = []
+
+    for i in range(-2, 3):
+        date = today + timedelta(days=i)
+        print(date.strftime('%A'))
+        dates.append({
+            'day': date.day,
+            'month': date.strftime('%b'),
+            'weekday': date.strftime('%A')
+        })
+
+    context = {
+        'dates': dates
+    }
+
+    return render(request, 'index.html', context)
